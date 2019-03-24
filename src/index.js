68f0f4bb27108125a94be4edac55d492601f92d6
darya990 <dasha.shibkova@mail.ru>
2019-03-24 18:23:16 +0300

    feat: count zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxNumber = Number.MAX_VALUE;
+
+  for(let i = 2; i <= base; i++){
+    if(base % i === 0){
+      let denominator = 0;
+      while(base % i === 0){
+        base /= i;
+        denominator++;
+      }
+
+      let result = Math.floor(number / i);
+      let count = 0;
+
+      while(result > 0){
+        count += result;
+        result = Math.floor(result / i);
+      }
+      maxNumber = Math.min(maxNumber, Math.floor(count / denominator));
+    }
+  }
+  return maxNumber;
 }
